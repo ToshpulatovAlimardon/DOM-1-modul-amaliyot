676e4e382dda967ace96e24293770ca6abfb0133
ToshpulatovAlimardon <alimardontoshpulatov001@gmail.com>
2024-06-03 15:03:37 +0500

    changed background

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 const advs = document.querySelectorAll(".promo__adv img"),
-  genre = document.querySelector(".promo__genre");
+  genre = document.querySelector(".promo__genre"),
+  wrapper = document.querySelector(".promo__bg");
 
 advs.forEach((item) => {
   item.remove();
 });
 
 genre.textContent = "COMEDY";
+
+wrapper.style.cssText = "background: url('../img/1.jpg') center bottom/cover no-repeat;"
