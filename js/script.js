aaca4440124cac0d9f1d6230f992e9d1b40e57a9
ToshpulatovAlimardon <alimardontoshpulatov001@gmail.com>
2024-06-08 12:47:01 +0500

    added domcontent

--- js/script.js
+++ js/script.js
@@ -1,32 +1,34 @@
-const advs = document.querySelectorAll(".promo__adv img"),
-  genre = document.querySelector(".promo__genre"),
-  wrapper = document.querySelector(".promo__bg"),
-  seriesList = document.querySelector(".promo__interactive-list");
+document.addEventListener("DOMContentLoaded", () => {
+  const advs = document.querySelectorAll(".promo__adv img"),
+    genre = document.querySelector(".promo__genre"),
+    wrapper = document.querySelector(".promo__bg"),
+    seriesList = document.querySelector(".promo__interactive-list");
 
-advs.forEach((item) => {
-  item.remove();
-});
+  advs.forEach((item) => {
+    item.remove();
+  });
 
-genre.textContent = "COMEDY";
+  genre.textContent = "COMEDY";
 
-wrapper.style.cssText =
-  "background: url('../img/1.jpg') center bottom/cover no-repeat;";
+  wrapper.style.cssText =
+    "background: url('../img/1.jpg') center bottom/cover no-repeat;";
 
-const seriesDb = {
-  series: [
-    "Omar",
-    "The Final Legacy",
-    "Ertugrul",
-    "Magnificent Century",
-    "The Great Seljuks: Guardians...",
-  ],
-};
+  const seriesDb = {
+    series: [
+      "Omar",
+      "The Final Legacy",
+      "Ertugrul",
+      "Magnificent Century",
+      "The Great Seljuks: Guardians...",
+    ],
+  };
 
-seriesDb.series.forEach((item, indx) => {
-  seriesList.innerHTML += `
+  seriesDb.series.forEach((item, indx) => {
+    seriesList.innerHTML += `
     <li class="promo__interactive-item">
       ${indx + 1}. ${item}
       <div class="delete"></div>
     </li>
   `;
+  });
 });
